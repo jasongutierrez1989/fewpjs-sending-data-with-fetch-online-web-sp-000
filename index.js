58b476512ce2e5b07dcbd783e4790a358fbfc790
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-22 06:40:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 function submitData(name, email) {
+<<<<<<< HEAD
   return fetch("http://localhost:3000/users", {
+=======
+  fetch("http://localhost:3000/users", {
+>>>>>>> 10a31f86501c1b7570137c833f95016929af12fd
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
+<<<<<<< HEAD
     },
+=======
+    }
+>>>>>>> 10a31f86501c1b7570137c833f95016929af12fd
     body: JSON.stringify({
       name: name,
       email: email
     })
+<<<<<<< HEAD
   })
   .then(function(response) {
     return response.json();
@@ -20,4 +29,7 @@ function submitData(name, email) {
   .catch(function(error) {
     console.log(error.message);
   })
+=======
+  };
+>>>>>>> 10a31f86501c1b7570137c833f95016929af12fd
 }
